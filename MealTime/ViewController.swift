7e722b162e65cb5ebc731f80ecedaa2dc4c47866
Rus <de.mr44@gmail.com>
2021-05-12 13:13:14 +0500

    Added save and delete functions

--- MealTime/ViewController.swift
+++ MealTime/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
     @IBOutlet var tableView: UITableView!
     
     var context: NSManagedObjectContext!
-    var array = [Date]()
+    var user: User!
     
     lazy var dateFormatter: DateFormatter = {
         let formatter = DateFormatter()
@@ -21,35 +21,80 @@ class ViewController: UIViewController {
     }()
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
-        let date = Date()
-        array.append(date)
-        tableView.reloadData()
+        
+        let meal = Meal(context: context)
+        meal.date = Date()
+        
+        let meals = user.meals?.mutableCopy() as? NSMutableOrderedSet
+        meals?.add(meal)
+        user.meals = meals
+        
+        do {
+            try context.save()
+            tableView.reloadData()
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        
+        let userName = "Max"
+        let fetchRequest: NSFetchRequest<User> = User.fetchRequest()
+        fetchRequest.predicate = NSPredicate(format: "name == %@", userName)
+        
+        do {
+            let results = try context.fetch(fetchRequest)
+            if results.isEmpty {
+                user = User(context: context)
+                user.name = userName
+                try context.save()
+            } else {
+                user = results.first
+            }
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
     }
 }
 
 // MARK: - UITableViewDataSource
 
 extension ViewController: UITableViewDataSource {
+    
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         return "My happy meal time"
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return array.count
+        return user.meals?.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as UITableViewCell
         
-        let date = array[indexPath.row]
+        guard let meal = user.meals?[indexPath.row] as? Meal,
+              let mealDate = meal.date
+              else { return cell }
         
-        cell!.textLabel!.text = dateFormatter.string(from: date)
-        return cell!
+        cell.textLabel!.text = dateFormatter.string(from: mealDate)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        guard let meal = user.meals?[indexPath.row] as? Meal, editingStyle == .delete else { return }
+        
+        context.delete(meal)
+        
+        do {
+            try context.save()
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
     }
 }
 
